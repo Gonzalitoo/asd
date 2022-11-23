63c0075c9a5271d9b483e5488789389611f40b76
Gonzalitoo <69219782+Gonzalitoo@users.noreply.github.com>
2022-11-23 09:53:41 -0300

    Update ActivitiesList.jsx

--- src/Components/Activities/ActivitiesList.jsx
+++ src/Components/Activities/ActivitiesList.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-
-import { ListCard } from "../Card/ListCard/ListCard";
 import { getActivities } from "../../Services/activitiesService/activitiesService";
 import Title from "../Title/Title";
 
