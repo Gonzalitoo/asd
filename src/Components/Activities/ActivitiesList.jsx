ab9095a64cd002c1f4e3676fdc4c4244a941259f
Gonzalitoo <69219782+Gonzalitoo@users.noreply.github.com>
2022-11-23 09:53:29 -0300

    Update ActivitiesList.jsx

--- src/Components/Activities/ActivitiesList.jsx
+++ src/Components/Activities/ActivitiesList.jsx
@@ -1,1 +1,26 @@
-//asd
+import React, { useEffect, useState } from "react";
+
+import { ListCard } from "../Card/ListCard/ListCard";
+import { getActivities } from "../../Services/activitiesService/activitiesService";
+import Title from "../Title/Title";
+
+import "../CardListStyles.css";
+
+const ActivitiesList = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    getActivities().then((response) => {
+      setActivities(response);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <Title title="Actividades" />
+      
+    </div>
+  );
+};
+
+export default ActivitiesList;
